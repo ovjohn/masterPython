903ab6552b64f044f03ac9edee00ebfcb32b6c81
John Olivares <olivaresvjohn@gmail.com>
2020-07-06 13:33:37 -0400

    Ejercicio.py

---
+++ 07-Ejercicios/ejercicio6.py
@@ -0,0 +1,6 @@
+#Tabla de Multiplicar del Uno al Diez 
+
+for num1 in range(1,11): 
+    print(f"\n##########Tabla de Multiplicar del {num1} ################")
+    for num2 in range(1,11):
+        print(f"{num1} x {num2} = {num1 * num2} ")
