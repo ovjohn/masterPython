b5f4b4d92d32e7be6907cc10caa8aef0e5ce33af
John Olivares <olivaresvjohn@gmail.com>
2020-08-13 14:50:18 -0400

    Ejercicio 2 Bucles

---
+++ 11-Ejercicios/2-ejercicio.py
@@ -0,0 +1,20 @@
+"""
+Hacer un programa, que añada valores a una lista
+mientras que su longitud sea menor a 120 y luego
+mostrar la lista por pantalla.
+Plus: Usar While y for
+"""
+"""
+lista =[]
+for i in range(0,121):
+    lista.append(f"elemento-{i}")
+    print("agergando el:"+ lista[i])
+print(lista)
+"""
+lista = []
+n = 0
+while n <= 120:
+    lista.append(f"elemento: {n}")
+    print("Añadiendiendo:" + lista[n])
+    n += 1
+print(lista)    
