e00ce9a6ab06c5b72b54e5c27b8405936f2cff83
John Olivares <olivaresvjohn@gmail.com>
2020-09-23 18:38:22 -0400

    Contnuacion de trabajo con ficheros

--- 14-sistema-archivos/fichero.py
+++ 14-sistema-archivos/fichero.py
@@ -1,5 +1,6 @@
 from io import open #Importando del paquete io el modulo open
 import pathlib
+import shutil #Modulo para copiar y pegar ficheros
 
 #Abrir archivo
 ruta = str(pathlib.Path().absolute()) + "/fichero-texto.txt"
@@ -10,3 +11,24 @@ archivo.write("*********Soy un texto creado por python************\n")
 
 #Cerrar archivo
 archivo.close()
+
+#Abrir archivo
+ruta = str(pathlib.Path().absolute()) + "/fichero-texto.txt"
+archivo_contenido = open(ruta,"r") #Con el atributo "r" para abrir solo lectura
+
+# Leer contenido
+#contenido = archivo_contenido.read() #Aqui se usa el metodo read para leer
+#print(type(contenido))
+
+#Leer contenido y guardar en Lista
+lista = archivo_contenido.readlines()
+archivo_contenido.close
+
+for e in lista:
+    print("- "+e.center(10))
+
+#Copiando Fichero
+rutaOriginal = str(pathlib.Path().absolute()) + "/fichero-texto.txt"
+rutaNueva = str(pathlib.Path().absolute()) + "/fichero-nuevo.txt"
+
+shutil.copyfile(rutaOriginal, rutaNueva) #modulo que sireve para copiar un fichero a otro lugar
