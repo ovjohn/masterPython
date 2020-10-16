81492b0c62d1ab31b1084f2dcf2caf2ce76c459f
John Olivares <olivaresvjohn@gmail.com>
2020-10-16 18:03:29 -0400

    Detectando el tipado de los Objetos

--- 17-POO-constructor/main.py
+++ 17-POO-constructor/main.py
@@ -4,3 +4,8 @@ auto = Coche("Rojo","Doge","Ram",190,6,4)
 
 #print(auto.getMarca())
 print(auto.getInfo())
+
+if type(auto) == Coche:
+    print("Auto es un Objeto de tipo Coche")
+else:
+    print("Auto no es de tipo coche")
